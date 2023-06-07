60a2f254aa4a70ee85668a4e6e705caaedd78e65
zineb790 <nohad91@hotmail.fr>
2023-06-06 20:43:36 -0400

    created hamster class

---
+++ homework.js
@@ -0,0 +1,16 @@
+class Hamster{
+    constructor(owner, name,price) {
+        this.owner = ""
+        this.name = name
+        this.price=15
+    }
+    wheelRun() {
+        console.log("squeak squeak")
+    }
+    eatFood() {
+        console.log("nibble nibble")
+    }
+    getPrice() {
+        return this.price
+    }
+}
