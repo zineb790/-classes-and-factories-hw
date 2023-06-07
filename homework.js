96f1ffca346aa329d84a235f9bf2d2b844c8caa8
zineb790 <nohad91@hotmail.fr>
2023-06-06 22:11:38 -0400

    created person class

--- homework.js
+++ homework.js
@@ -1,3 +1,4 @@
+//hamster
 class Hamster{
     constructor(owner, name,price) {
         this.owner = ""
@@ -13,4 +14,56 @@ class Hamster{
     getPrice() {
         return this.price
     }
+}
+const hamster1= new Hamster("me","hamstry")
+hamster1.owner="me"
+console.log(hamster1)
+//person
+class Person{
+    constructor(name,age,height,weight,mood,hamsters,bankAccount) {
+        this.name = name
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = mood
+        this.hamsters = []
+        this.bankAccount = 0
+        
+    }
+    getName() {
+        return this.name
+    }
+    getAge() {
+       return this.age 
+    }
+    getWeight() {
+        return this.weight
+    }
+    greet() {
+        console.log(this.name)
+    }
+    eat() {
+        this.weight++
+        this.mood++
+    }
+    exercise() {
+        this.weight--
+    }
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount+=10
+
+    }
+    buyHamster(hamster) {
+        this.hamsters.push(hamster1)
+        this.mood += 10
+        this.bankAccount-= this.getPrice
+        
+
+        
+    }
+    
 }
